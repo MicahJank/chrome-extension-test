dbc90eae8f73b9dcbca53b41f2f0c0926ff3ee84
MicahJank <MicahJank@users.noreply.github.com>
2020-11-07 00:25:16 -0600

    refactored the content.js file to only log the url when the tab gets the correct message sent to it

--- content.js
+++ content.js
@@ -1,3 +1,13 @@
-const firstHref = $("a[href^='http']").eq(0).attr("href");
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        // now the console log only runs when the correct message is recieved
+        // which if we set up the background.js correctly should happen
+        // every time we click on the extension
+        if (request.message === "clicked_browser_action") {
+            const firstHref = $("a[href^='http']").eq(0).attr("href");
+            console.log(firstHref);
 
-console.log(firstHref);
+        }
+    }
+)
+
